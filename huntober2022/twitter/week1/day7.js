db71f18a08ddecd9083a4918f9fe3f67500f5b8f
onyxV <averymc.dev@gmail.com>
2022-07-17 09:49:45 -0500

    Restructure directory

---
+++ huntober2022/twitter/week1/day7.js
@@ -0,0 +1,21 @@
+/* Today you'll need to create a function that swaps each letter of the alphabet for its opposite. A letter's opposite is one that, if the alphabet were flipped Z-A, would be the same number of letters in, and which would also have the opposite case. */
+
+function swap(str){
+  console.log(str)
+  let alpha = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'].toString().split('')
+  str.split('')
+  // loop through str to check letters
+  for(let i=0; i<str.length; i++){
+  
+    // change letter to other one
+    if(str[i] == str[i].toLowerCase()){
+      console.log(str[i])
+      
+    }
+    // change casing to other one
+    
+  }
+  // join string
+}
+
+swap('vCZNKOV: 0 MFNYVI LI KFMXGFZGRLM XSZMTVH')
