13a80d84da8ce04d60928d97f602f0ae05e5c42e
Avery McLaughlin <averymc.dev@gmail.com>
2022-09-25 10:01:20 -0500

    Optimized doubling function for speed and efficiency

---
+++ codewars/8kyu/integerArrayDoubled.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    return x.map(e => e * 2)
+}
+
+maps([2,2,2])
