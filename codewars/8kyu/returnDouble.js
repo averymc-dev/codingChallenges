a164832818e5f129e06b12a84cc336b7bf284a69
Avery McLaughlin <averymc.dev@gmail.com>
2022-09-27 08:13:15 -0500

    Refactored doubling function by changing it into an expression

---
+++ codewars/8kyu/returnDouble.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+
+// REFACTORED SOLUTION (2/12/2023)
+const getDouble = i => i * 2
