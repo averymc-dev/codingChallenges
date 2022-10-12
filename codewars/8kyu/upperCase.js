669aa29cae360445d02dbfc4dc79ed196b12ccb4
Avery McLaughlin <averymc.dev@gmail.com>
2022-10-12 10:54:08 -0500

    Refactored uppercase function

---
+++ codewars/8kyu/upperCase.js
@@ -0,0 +1,7 @@
+//Write a function which converts the input string to uppercase.
+
+const makeUpperCase = (str) => str.toUpperCase()
+
+// REFACTORED 2/16/2023
+
+const upperCase = str => str.toUpperCase()
