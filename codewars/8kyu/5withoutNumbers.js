6d5b6d6e821d8eaf9fc429b19e70f2ce34931b7f
Avery McLaughlin <averymc.dev@gmail.com>
2022-11-09 07:33:08 -0600

    Always returns five

---
+++ codewars/8kyu/5withoutNumbers.js
@@ -0,0 +1,11 @@
+// Write a function that always returns 5. Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+const unusualFive = (n) => {
+  return 'easy!'.length
+}
+
+unusualFive(5)
+
+// REFACTOR
+
+const unusualFive = n => 'easy!'.length
