9c9a60c0a8da8c277fdee4672487979a3e15ea2d
averymc-dev <averymc.dev@gmail.com>
2022-11-19 10:46:10 -0600

    Converts a binary number string to a decimal

--- codewars/8kyu/binToDecimal.js
+++ codewars/8kyu/binToDecimal.js
@@ -1,1 +1,6 @@
 //Complete the function which converts a binary number (given as a string) to a decimal number.
+
+const binToDec = (bin) => parseInt(bin,2)
+
+console.log(binToDec(1), 1)
+console.log(binToDec("1001001"), 73)
