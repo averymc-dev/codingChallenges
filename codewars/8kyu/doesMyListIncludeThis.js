227b02779bf1f9248b82973673e1a4d945221061
averymc-dev <averymc.dev@gmail.com>
2022-11-24 06:39:53 -0600

    Finds if item is included in array

---
+++ codewars/8kyu/doesMyListIncludeThis.js
@@ -0,0 +1,4 @@
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+const include = (arr, item) => arr.includes(item)
+
+console.log(include([1,2,3,4], 3), true) // true, true
