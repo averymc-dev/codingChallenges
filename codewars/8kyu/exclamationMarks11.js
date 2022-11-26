d0a27b74dc35e8e30fd54803311b8808cbc994dd
averymc-dev <averymc.dev@gmail.com>
2022-11-26 10:27:27 -0600

    Replaces vowels with exclamation marks

---
+++ codewars/8kyu/exclamationMarks11.js
@@ -0,0 +1,8 @@
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+const replace = s => {
+  console.log(s)
+  let vowel = ['a','e','i','o','u','A','E','I','O','U']
+  // Regex? Please
+}
+
+console.log(replace('Hi!'), 'H!!')
