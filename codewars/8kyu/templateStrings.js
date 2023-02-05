3af7f7418c9ff136c82ef291a6db084bb162de23
averymc-dev <averymc.dev@gmail.com>
2023-02-05 14:31:23 -0600

    Returns a string using template literals

---
+++ codewars/8kyu/templateStrings.js
@@ -0,0 +1,10 @@
+/*Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '``` */
+
+const templateStrings = (noun, adjective) => `${noun} are ${adjective}`
+
+templateStrings('Animals', 'Good')
