537831c9aa549f88429a89ea1e18bb1067a060d4
averymc-dev <averymc.dev@gmail.com>
2023-02-09 20:35:45 -0600

    Returns the amount of positive odd numbers below a given number

---
+++ codewars/8kyu/countOddNumbersBelowN.js
@@ -0,0 +1,9 @@
+/*Given a number n, return the number of positive odd numbers below n, EASY!
+
+Expect large Inputs!*/
+
+const oddCount = n => Math.floor(n/2)
+
+
+oddCount(15) // 7
+oddCount(15023) // 7511
