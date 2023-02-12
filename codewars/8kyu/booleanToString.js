72ba7c5fbb5ce7ebf4bb5e621be811e45e2177b1
Avery McLaughlin <averymc.dev@gmail.com>
2023-02-12 07:06:42 -0600

    Refactored boolean to string conversion function for speed and efficiency

---
+++ codewars/8kyu/booleanToString.js
@@ -0,0 +1,11 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    //your code here
+    return String(b)
+  }
+
+// Second solution (2/12/2023)
+const makeStringFromBoolean = b => String(b)
