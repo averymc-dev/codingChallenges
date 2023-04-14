0874f8ae36a0467305f0cded218d933d8b50b3c1
averymc-dev <averymc.dev@gmail.com>
2023-04-14 13:49:44 -0500

    Takes an input and returns a number or will return null if conversion is not possible

---
+++ codewars/8kyu/parseFloat.js
@@ -0,0 +1,12 @@
+/*Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.*/
+
+function parseF(s) {
+  if(s == String(s)){
+    return parseFloat(s)
+  } else { return null }
+}
+
+parseF("1") // 1.0
+parseF("0") // 0
+
+//INCOMPLETE
