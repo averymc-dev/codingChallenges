137080435354b7a5b7ec2488101d5fb0faba3962
averymc-dev <averymc.dev@gmail.com>
2023-04-16 11:03:20 -0500

    Confirms if the variable type matches the given type

---
+++ codewars/8kyu/forTwins1-Types.js
@@ -0,0 +1,10 @@
+/*This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
+Task:
+
+Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.*/
+
+function typeValidation(variable, type) {
+  return typeof(variable) == type
+}
+
+typeValidation(42, "number") // true
