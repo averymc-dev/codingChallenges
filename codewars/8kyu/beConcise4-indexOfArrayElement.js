09aede884d42fbae7f01e9589c60f2bc792c3bc8
averymc-dev <averymc.dev@gmail.com>
2023-04-25 12:51:28 -0500

    Finds the index of an array element or a string if it is not found

---
+++ codewars/8kyu/beConcise4-indexOfArrayElement.js
@@ -0,0 +1,18 @@
+/*Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
+
+function find(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) return i;
+  }
+  return "Not found";
+}*/
+
+const find = (arr, el) => arr.indexOf(el)
+
+var array = [2,3,5,7,11];
+find(array, 5) // 2
+find(array, 11) // 4
+find(array, 2) // 0
+find(array, 1) // "Not found"
+
+//INCOMPLETE
