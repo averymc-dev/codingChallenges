346ddb2724817fc11b44e31eb66de291bcf256b0
averymc-dev <averymc.dev@gmail.com>
2023-10-22 15:26:24 -0500

    Determines which quadrant a point exists in a Euclidean plane, given x and y coordinates

---
+++ codewars/8kyu/quadrants.js
@@ -0,0 +1,17 @@
+/*Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.*/
+
+const quadrant = (x,y) => x > 0 && y > 0 ? 1 : x < 0 && y > 0 ? 2 : x < 0 && y < 0 ? 3 : x > 0 && y < 0 ? 4 : 0
+
+quadrant(1, 2) // 1
+quadrant(3, 5) // 1
+quadrant(-10, 100) // 2
+quadrant(-1, -9) // 3
+quadrant(19, -56) // 4
+
+/*TOP SOLUTION
+function quadrant(x, y) {
+  if (x > 0 && y > 0) return 1;
+  if (x < 0 && y > 0) return 2;
+  if (x < 0 && y < 0) return 3;
+  return 4;
+}*/
