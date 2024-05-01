bac93f53dee9dad5892cb48d0e26db9e0bad39cf
averymc-dev <averymc.dev@gmail.com>
2024-05-01 14:46:12 -0500

    Finds the even numbers in an array and returns the last end of the array based on the number passed in

---
+++ codewars/7kyu/evenNumbersInAnArray.js
@@ -0,0 +1,26 @@
+/*Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+For example:
+
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+*/
+
+function evenNumbers(array, number) {
+  let result = []
+  array = array.reverse()
+  for(let i=0; i<array.length; i++){
+    if(result.length < number && array[i] % 2 == 0){
+      result.push(array[i])
+    }
+  }
+  return result.reverse()
+}
+
+evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) // [4, 6, 8]
+evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) // [-8, 26]
+
+/*TOP SOLUTION
+const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);*/
