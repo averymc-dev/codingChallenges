684d42b575b436e1e8d994811460a5d839a0d597
averymc-dev <averymc.dev@gmail.com>
2024-08-27 17:46:31 -0500

    Creates an array for all the rooms in escape game

---
+++ codewars/8kyu/grasshopper-createTheRooms.js
@@ -0,0 +1,39 @@
+/*You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+*/
+
+let rooms = [
+  {
+    "name": "name",
+    "description": "description",
+    "completed": false
+  },
+  {
+    "name": "name",
+    "description": "description",
+    "completed": false
+  },
+  {
+    "name": "name",
+    "description": "description",
+    "completed": false
+  }
+]
+
+/*TOP SOLUTION
+const rooms = {
+  bedroom: {
+    name: 'Bedroom',
+    description: 'For... sleeping',
+    completed: true,
+  },
+  bathroom: {
+    name: 'Bathroom',
+    description: 'This will get you clean',
+    completed: false,
+  },
+  kitchen: {
+    name: 'Kitchen',
+    description: 'Hungry? Why wait?',
+    completed: true,
+  },
+}*/
