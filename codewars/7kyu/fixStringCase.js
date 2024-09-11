0d7da50749a07f4a621124f5eca972796337ccd4
averymc-dev <averymc.dev@gmail.com>
2024-09-11 18:08:09 -0500

    Changes case of string based on which case has more letters

---
+++ codewars/7kyu/fixStringCase.js
@@ -0,0 +1,24 @@
+/*In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+    make as few changes as possible.
+    if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+
+For example:
+
+solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+More examples in test cases. Good luck!*/
+
+function solve(s){
+  let upperCount = 0
+  let lowerCount = 0
+  s.split('').forEach((c) => {
+    c == c.toUpperCase() ? upperCount++ : lowerCount ++
+  })
+  return lowerCount >= upperCount ? s.toLowerCase() : s.toUpperCase()
+}
+
+solve("CODe") // "CODE"
+solve("COde") // "code"
