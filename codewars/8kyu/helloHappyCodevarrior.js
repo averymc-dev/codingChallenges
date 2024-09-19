84436fc199c4b0ebce41c0268e624fb484779701
averymc-dev <averymc.dev@gmail.com>
2024-09-19 18:34:57 -0500

    Fixes an object to display the correct name

---
+++ codewars/8kyu/helloHappyCodevarrior.js
@@ -0,0 +1,31 @@
+/*VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+*/
+
+function Warrior(n){
+  let name = n;  
+  this.name = function(n){
+    if( n ) name=n;
+    return name;
+  }
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
+
+var albert = new Warrior("Albert")
+var boris  = new Warrior("Boris")
+
+albert.toString() // "Hi! my name's Albert"
+albert.name() // "Albert"
+boris.name() // "Boris"
