2167efea0c4978c869c4f2ac2a63fed78862cad0
averymc-dev <averymc.dev@gmail.com>
2025-03-17 15:57:40 -0500

    Test for a and NOT a both equal to false

---
+++ codewars/8kyu/a==NOTa.js
@@ -0,0 +1,16 @@
+/*You task to pass only this tests :
+
+    a == false
+    !a == false
+    a == !a
+
+*/
+
+const a = false
+
+a == false //  true
+!a == false // true
+a == !a //     true
+
+/*TOP SOLUTION
+const a = []*/
