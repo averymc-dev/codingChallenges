c3d3ac1d7487d7859e3e14e8107461572c2e2609
averymc-dev <averymc.dev@gmail.com>
2025-04-21 18:21:40 -0500

    Determines if an array of numbers is descending, ascending, or not and outputs a string with its status

---
+++ codewars/7kyu/sortedYesNoHow.js
@@ -0,0 +1,34 @@
+/* Complete the method which accepts an array of integers, and returns one of the following:
+
+    "yes, ascending" - if the numbers in the array are sorted in an ascending order
+    "yes, descending" - if the numbers in the array are sorted in a descending order
+    "no" - otherwise
+
+You can assume the array will always be valid, and there will always be one correct answer.
+*/
+
+function isSortedAndHow(array) {
+  let isAscending = true;
+  let isDescending = true;
+  
+  for(let i=1; i<array.length; i++){
+    if(array[i] > array[i - 1]){
+      isDescending = false
+    }else if(array[i] < array[i - 1]){
+      isAscending = false
+    }
+  }
+  return isAscending ? 'yes, ascending' : isDescending ? 'yes, descending' : 'no'
+}
+
+function isAscending()
+
+isSortedAndHow([1, 2]) // 'yes, ascending'
+isSortedAndHow([15, 7, 3, -8]) // 'yes, descending'
+isSortedAndHow([4, 2, 30]) // 'no'
+
+/*TOP SOLUTION
+function isSortedAndHow(arr) {
+  return arr.every((x,i)=>i==0||arr[i]>=arr[i-1])?'yes, ascending':
+         arr.every((x,i)=>i==0||arr[i]<=arr[i-1])?'yes, descending':'no'
+}*/
