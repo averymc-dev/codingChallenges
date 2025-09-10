c1f7b3cdfee849e8831dc0e570bfaa1d6817825e
averymc-dev <averymc.dev@gmail.com>
2025-09-10 13:39:28 -0500

    Update solution to use string prototypes

--- codewars/7kyu/jadenCasingStrings.js
+++ codewars/7kyu/jadenCasingStrings.js
@@ -22,3 +22,7 @@ var str = "How can mirrors be real if our eyes aren't real";
 str.toJadenCase() // "How Can Mirrors Be Real If Our Eyes Aren't Real")
 
 //INCOMPLETE
+/*UPDATED SOLUTION
+String.prototype.toJadenCase = function(){
+  return this.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+}*/
